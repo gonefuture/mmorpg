cd4dfb586fa9b5ce33e34eee63c487d16b116042
gonefuture <gonefuture@qq.com>
2018-12-03 17:50:26 +0800

    移除物品时去除物品属性增益。装备已经有装备的部位时，将装备放回背包

---
+++ gameserver/src/main/java/com/wan37/gameServer/game/bag/controller/RemoveEquipController.java
@@ -0,0 +1,10 @@
+package com.wan37.gameServer.game.bag.controller;
+
+/**
+ * @author gonefuture  gonefuture@qq.com
+ * time 2018/12/3 15:18
+ * @version 1.00
+ * Description: mmorpg
+ */
+public class RemoveEquipController {
+}
