8c1e7cab7c11c17ea75844179e01a8a42a70f299
gonefuture <gonefuture@qq.com>
2018-12-12 18:14:27 +0800

    副本boos守关和攻击玩家

---
+++ gameserver/src/main/java/com/wan37/gameServer/game/gameSceneObject/service/MonsterAIService.java
@@ -0,0 +1,10 @@
+package com.wan37.gameServer.game.gameSceneObject.service;
+
+/**
+ * @author gonefuture  gonefuture@qq.com
+ * time 2018/12/12 14:22
+ * @version 1.00
+ * Description: mmorpg
+ */
+public class Monster {
+}
