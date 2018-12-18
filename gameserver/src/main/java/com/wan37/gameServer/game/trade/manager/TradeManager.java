249c0292a76c747695b9e9751469cb315758e60e
gonefuture <gonefuture@qq.com>
2018-12-18 20:01:44 +0800

    发起交易请求和接受交易

---
+++ gameserver/src/main/java/com/wan37/gameServer/game/trade/manager/TradeManager.java
@@ -0,0 +1,17 @@
+package com.wan37.gameServer.game.trade;
+
+/**
+ * @author gonefuture  gonefuture@qq.com
+ * time 2018/12/18 17:09
+ * @version 1.00
+ * Description: 交易管理
+ */
+public class TradeManager {
+
+
+
+
+
+
+    
+}
