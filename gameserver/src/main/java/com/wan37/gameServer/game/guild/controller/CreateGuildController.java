1f96bad1875a1f849a7acf159a008c2c1d141411
gonefuture <gonefuture@qq.com>
2018-12-19 16:52:37 +0800

    完善玩家交易的功能

---
+++ gameserver/src/main/java/com/wan37/gameServer/game/guild/controller/CreateGuildController.java
@@ -0,0 +1,10 @@
+package com.wan37.gameServer.game.guild.controller;
+
+/**
+ * @author gonefuture  gonefuture@qq.com
+ * time 2018/12/19 16:47
+ * @version 1.00
+ * Description: mmorpg
+ */
+public interface CreateGuildController {
+}
