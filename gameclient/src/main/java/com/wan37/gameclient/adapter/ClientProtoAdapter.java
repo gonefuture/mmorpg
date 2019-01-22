5ba6c263efdee03519af3bde96e886451cfdc1c6
gonefuture <gonefuture@qq.com>
2019-01-22 11:01:53 +0800

    将原本的自定义协议改成protobuf传输，删除与之前协议无关的东西

---
+++ gameclient/src/main/java/com/wan37/gameclient/adapter/ClientProtoAdapter.java
@@ -0,0 +1,34 @@
+package com.wan37.gameclient.adapter;
+
+import com.wan37.common.proto.CmdProto;
+import com.wan37.gameclient.view.MainView;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author gonefuture  gonefuture@qq.com
+ * time 2019/1/21 14:38
+ * @version 1.00
+ * Description: mmorpg
+ */
+
+@Slf4j
+public class ClientPoHandlerProto extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        CmdProto.Cmd message = (CmdProto.Cmd) msg;
+
+        MainView.output.append(message.getContent() + "\n");
+        // 使用JTextArea的setCaretPosition();手动设置光标的位置为最后一行。人气颇高。使用方法也很简单
+        MainView.output.setCaretPosition(MainView.output.getDocument().getLength());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
---
+++ gameserver/src/main/java/com/wan37/gameserver/server/adapter/ServerProtoAdapter.java
@@ -0,0 +1,107 @@
+package com.wan37.gameserver.server.adapter;
+
+import com.wan37.common.entity.Message;
+import com.wan37.common.proto.CmdProto;
+import com.wan37.gameserver.common.ErrorController;
+import com.wan37.gameserver.common.IController;
+import com.wan37.gameserver.game.player.service.PlayerQuitService;
+import com.wan37.gameserver.manager.controller.ControllerManager;
+import com.wan37.gameserver.manager.notification.NotificationManager;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * @author gonefuture  gonefuture@qq.com
+ * time 2019/1/21 12:26
+ * @version 1.00
+ * Description: mmorpg
+ */
+
+@Slf4j
+@ChannelHandler.Sharable
+@Component
+public class ServerPoHandlerProto  extends ChannelInboundHandlerAdapter {
+
+
+    @Resource
+    private ControllerManager controllerManager;
+
+    @Resource
+    private ErrorController errorController;
+
+    @Resource
+    private PlayerQuitService playerQuitService;
+
+
+
+    /**
+     *  当客户端连上服务器的时候触发此函数
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        log.info("客户端: " + ctx.channel().id() + " 加入连接", CharsetUtil.UTF_8);
+    }
+
+    /**
+     * 当客户端断开连接的时候触发函数
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+
+        ctx.writeAndFlush("正在断开连接");
+
+        // 将角色信息保存到数据库
+        playerQuitService.savePlayer(ctx);
+
+        // 清除缓存
+        playerQuitService.cleanPlayerCache(ctx);
+        log.info("客户端: " + ctx.channel().id() + " 已经离线");
+
+    }
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object message) {
+        CmdProto.Cmd cmd = (CmdProto.Cmd) message;
+        Message msg = new Message();
+        msg.setMsgId(cmd.getMgsId());
+        msg.setContent(cmd.getContent());
+        IController controller = controllerManager.get(cmd.getMgsId());
+        if (controller == null) {
+            errorController.handle( ctx ,msg);
+        } else {
+            controllerManager.execute(controller,ctx,msg);
+        }
+    }
+
+
+
+
+
+
+    /**
+     *  玩家意外退出时保存是数据
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)  {
+
+        NotificationManager.notifyByCtx(ctx,"出现了点小意外"+cause.getMessage());
+        log.error("服务器内部发生错误");
+
+        // 将角色信息保存到数据库
+        playerQuitService.savePlayer(ctx);
+
+        log.error("发生错误 {}", cause.getMessage());
+
+        // 打印错误
+        cause.printStackTrace();
+
+    }
+
+}
